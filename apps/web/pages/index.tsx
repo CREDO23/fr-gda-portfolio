3a6879dc6e22e6dc2cf5e88352f2988254c397bf
CREDO23 <thierrybakera12@gmail.com>
2023-05-21 12:07:02 +0200

    chore: add a tailwindcss config for all apps/packages

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+import {Button} from "@smarket-design"
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <Button />
+      <Button/>
     </div>
   );
 }
---
+++ packages/@tailwind-config/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "../../apps/**/*.{tsx,jsx}",
+        "../../packages/**/*.{tsx,jsx}"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
