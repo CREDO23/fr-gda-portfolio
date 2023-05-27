9fdcad031b8133f7be485b8e13dd6c20f5808d56
CREDO23 <thierrybakera12@gmail.com>
2023-05-27 13:32:18 +0200

    rebase the chore branch

--- packages/@smarket-design/input/index.tsx
+++ packages/@smarket-design/input/index.tsx
@@ -32,39 +32,3 @@ export default function Input({
     />
   );
 }
-import { Input as Inpt } from "@material-tailwind/react";
-import { IInputProps } from "..";
-
-export default function Input({
-  variant = 'outlined', 
-  size = 'md',
-  color = 'deep-orange',
-  label,
-  error = false,
-  success = false,
-  icon = null,
-  labelProps = undefined,
-  containerProps = undefined,
-  className = '',
-  shrink = undefined ,
-  inputRef = undefined,
-  type
-}: IInputProps) {
-  return (
-    <Inpt
-      variant={variant}
-      type  = {type}
-      size={size}
-      color={color}
-      label={label}
-      error={error}
-      success={success}
-      icon={icon}
-      labelProps={labelProps}
-      containerProps={containerProps}
-      className={className}
-      shrink={shrink}
-      inputRef={inputRef}
-    />
-  );
-}
